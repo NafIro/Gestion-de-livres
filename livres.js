7651d62add564ee460ff61f33f8edcfd2d342a5b
nafi-code <59339729+nafi-code@users.noreply.github.com>
2019-12-29 13:34:04 -0500

    Add files via upload

---
+++ livres.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById('enreg').onclick = function edition(){
+		document.getElementsByClassName("p1").[0].innerHTML = document.getElementById('titLiv').value
+		document.getElementsByClassName("p2").[0].innerHTML = document.getElementById('isbn').value
+		document.getElementsByClassName("p3").[0].innerHTML = document.getElementById('anEdition').value
+		document.getElementsByClassName("p4").[0].innerHTML = document.getElementById('desc').value
+
+
+}
